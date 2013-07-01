46c6737ffa58e8ee69bd1afd4ffe062cf7674f26
Théo LUBERT <theo.lubert@gmail.com>
2013-06-30 17:18:52 -0700

    File list test fix

--- Sources/Models/Requests/WRequest+List.m
+++ Sources/Models/Requests/WRequest+List.m
@@ -70,13 +70,14 @@
     }];
 }
 
-+ (void)markFileAsFavorite:(NSNumber *)idNumber
-                   success:(void (^)(id json))success
-                   failure:(void (^)(id error))failure
++ (void)markFile:(NSNumber *)idNumber
+      asFavorite:(NSString *)favorite
+         success:(void (^)(id json))success
+         failure:(void (^)(id error))failure
 {
     NSString *path = [@"/users/favorites/{id}" stringByReplacingOccurrencesOfString:@"{id}" withString:[idNumber description]];
     
-    [[WRequest client] postPath:path parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [[WRequest client] postPath:path parameters:@{@"favorite": favorite} success:^(AFHTTPRequestOperation *operation, id responseObject) {
         id json = [WRequest JSONFromData:responseObject];
         if ([json isKindOfClass:[NSError class]]) {
             failure([WRequest displayError:(NSError *)json forOperation:operation]);
@@ -94,11 +95,18 @@
     }];
 }
 
++ (void)markFileAsFavorite:(NSNumber *)idNumber
+                   success:(void (^)(id json))success
+                   failure:(void (^)(id error))failure
+{
+    [WRequest markFile:idNumber asFavorite:@"true" success:success failure:failure];
+}
+
 + (void)unmarkFileAsFavorite:(NSNumber *)idNumber
                      success:(void (^)(id json))success
                      failure:(void (^)(id error))failure
 {
-    [WRequest markFileAsFavorite:idNumber success:success failure:failure];
+    [WRequest markFile:idNumber asFavorite:@"false" success:success failure:failure];
 }
 
 @end
